5b5bda7ea5f861b7894418b0ff7cbc6d9e02eef6
David Nath <32259393+davidcnath@users.noreply.github.com>
2025-09-21 14:31:42 +0300

    Update wikipedia.js

--- src/services/wikipedia.js
+++ src/services/wikipedia.js
@@ -1,1 +1,4 @@
+// Wikipedia adapter.
+// Exports: search(query), listPages(topicOrUrl), fetchExtract(pageid), fetchInfobox(title), fetchPageviews(title)
+// All functions should accept { signal } for abort/timeouts.
 
